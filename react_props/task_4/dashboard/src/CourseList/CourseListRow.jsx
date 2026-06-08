678b90ea954773496a49e316902c49fcc23548c3
BrigidAn <ochimerema@gmail.com>
2026-06-08 12:41:15 +0200

    CourseList & Courseluistrow

--- react_props/task_4/dashboard/src/CourseList/CourseListRow.jsx
+++ react_props/task_4/dashboard/src/CourseList/CourseListRow.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function CourseListRow({ isHeader = false, textFirstCell = "", textSecondCell = null }) {
+function CourseListRow({
+  isHeader = false,
+  textFirstCell = "",
+  textSecondCell = null,
+}) {
   if (isHeader) {
     return (
       <tr>
