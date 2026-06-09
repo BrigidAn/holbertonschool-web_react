89cfa1d603c0a944723c1ea39c6a77e083ad5b23
BrigidAn <ochimerema@gmail.com>
2026-06-09 10:13:01 +0200

    Switch to class components

--- react_component/task_0/dashboard/src/App/App.jsx
+++ react_component/task_0/dashboard/src/App/App.jsx
@@ -5,15 +5,31 @@ import Login from "../Login/Login";
 import Footer from "../Footer/Footer";
 import CourseList from "../CourseList/CourseList";
 
+const notificationsList = [
+  { id: 1, type: "default", value: "New course available" },
+  { id: 2, type: "urgent", value: "New resume available" },
+  {
+    id: 3,
+    type: "urgent",
+    html: { __html: "<strong>Urgent requirement</strong> - complete by EOD" },
+  },
+];
+
+const coursesList = [
+  { id: 1, name: "ES6", credit: 60 },
+  { id: 2, name: "Webpack", credit: 20 },
+  { id: 3, name: "React", credit: 40 },
+];
+
 class App extends Component {
   render() {
     const isLoggedIn = false;
 
     return (
       <Fragment>
-        <Notifications />
+        <Notifications displayDrawer={true} notifications={notificationsList} />
         <Header />
-        {isLoggedIn ? <CourseList /> : <Login />}
+        {isLoggedIn ? <CourseList courses={coursesList} /> : <Login />}
         <Footer />
       </Fragment>
     );
