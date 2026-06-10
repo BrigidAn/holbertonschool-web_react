0ec6e2f952ddd5b977ecf9c3ae49a0778155b81e
BrigidAn <ochimerema@gmail.com>
2026-06-10 11:41:03 +0200

    header modified

--- react_styling/task_1/dashboard/src/Header/Header.jsx
+++ react_styling/task_1/dashboard/src/Header/Header.jsx
@@ -1,16 +1,21 @@
 import React from "react";
-import holbertonlogo from "../assets/holberton-logo.jpg"
+import holbertonlogo from "../assets/holberton-logo.jpg";
 
-const Header = () => {
+function Header() {
   return (
-    <header className="App-header">
-      <img 
-      src={holbertonlogo}
-      alt="holberton logo" 
-      className="App-logo" />
-      <h1>School dashboard</h1>
+    <header className="App-header flex items-center">
+      <img src={holbertonlogo} alt="holberton logo" className="App-logo h-16" />
+
+      <h1
+        className="text-3xl font-bold"
+        style={{
+          color: "var(--main-color)",
+        }}
+      >
+        School dashboard
+      </h1>
     </header>
   );
-};
+}
 
 export default Header;
